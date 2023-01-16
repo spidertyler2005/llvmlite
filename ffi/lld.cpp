97b0fa015c048ea77e0d09cabb508e9a83a9a4bd
Benjamin Austin Jr <spidertyler1122@gmail.com>
2023-01-16 01:08:16 -0500

    Formatting for lld.cpp and use API_EXPORT

--- ffi/lld.cpp
+++ ffi/lld.cpp
@@ -4,16 +4,16 @@
 
 extern "C" {
 
-bool lld_main(int Argc, const char **Argv, const char **outstr) {
-    //InitLLVM X(Argc, Argv);
+API_EXPORT(bool) lld_main(int Argc, const char **Argv, const char **outstr) {
+    // InitLLVM X(Argc, Argv);
     std::string command_output;
     llvm::raw_string_ostream command_stream(command_output);
     std::vector<const char *> Args(Argv, Argv + Argc);
 
-    bool linker_output = !lld::elf::link(Args, false, command_stream, command_stream);
+    bool linker_output =
+        !lld::elf::link(Args, false, command_stream, command_stream);
 
     *outstr = LLVMPY_CreateString(command_output.c_str());
-
     return linker_output;
 }
 
